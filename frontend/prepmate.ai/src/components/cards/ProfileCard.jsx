483433be78a94ab038ee814034fccc49209fcc94
sudip <sudipjana0702@gmail.com>
2025-08-04 20:20:45 +0530

    correct profileImageUrl issue

--- frontend/prepmate.ai/src/components/cards/ProfileCard.jsx
+++ frontend/prepmate.ai/src/components/cards/ProfileCard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../Context/userContext";
 import { useNavigate } from "react-router-dom";
+import {User} from 'lucide-react';
 
 const ProfileInfoCard = () => {
     const { user, clearUser } = useContext(UserContext);
@@ -17,11 +18,17 @@ const ProfileInfoCard = () => {
 
     return (
         <div className="flex items-center">
-            <img
-                src={user.data.profileImageUrl}
-                alt="Profile"
-                className="w-11 h-11 bg-gray-300 rounded-full mr-3"
-            />
+            {user?.data?.profileImageUrl ? (
+  <img
+    src={user.data.profileImageUrl}
+    alt="Profile"
+    className="w-11 h-11 bg-gray-300 rounded-full mr-3 object-cover"
+  />
+) : (
+  <div className="w-11 h-11 bg-gray-300 rounded-full mr-3 flex items-center justify-center">
+    <User className="w-6 h-6 text-gray-500" />
+  </div>
+)}
             <div>
                 <div className="text-[15px] text-black font-bold leading-3">
                     {user.name || ""}
